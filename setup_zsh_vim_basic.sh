5c0d1f419202bdee6460fc779e6caad72564ed4f
Raymond <raymond.y.xu@gmail.com>
2020-08-05 22:36:55 +0000

    :smile:

--- setup_zsh_vim_basic.sh
+++ setup_zsh_vim_basic.sh
@@ -2,6 +2,12 @@ sudo add-apt-repository ppa:bashtop-monitor/bashtop
 sudo apt update
 sudo apt install zsh
 
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "run :PlugInstall" in vim
+
 git clone https://github.com/ryx2/rc.git
 cp rc/vimrc .vimrc
 cp rc/zshrc .zshrc
@@ -9,16 +15,13 @@ cp rc/zshrc .zshrc
 git clone https://github.com/ryx2/ohmyzsh.git
 mv ohmyzsh .oh-my-zsh
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "run :PlugInstall" in vim
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+sudo apt install python3-pip bashtop htop neofetch docker.io vim-gtk3 npm
 
-sudo apt install python3-pip bashtop htop neofetch docker.io vim-gtk3
+sudo npm i -g gitmoji-cli
 
 zsh
 
