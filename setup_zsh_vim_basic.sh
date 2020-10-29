5a4be707f9a25a13a5d77bdf40d46b83b01d2e37
Raymond Xu <raymond.y.xu@gmail.com>
2020-10-29 08:03:04 +0000

    :bug: fixed install script

--- setup_zsh_vim_basic.sh
+++ setup_zsh_vim_basic.sh
@@ -1,29 +1,29 @@
-sudo add-apt-repository ppa:bashtop-monitor/bashtop
+cd
 sudo apt update
-sudo apt install zsh
+sudo apt install zsh python3-pip bashtop htop neofetch vim-gtk3 npm fonts-powerline cmake
 
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "run :PlugInstall" in vim
 
 git clone https://github.com/ryx2/rc.git
-cp rc/vimrc .vimrc
-cp rc/zshrc .zshrc
+rm .vimrc
+ln -s rc/vimrc .vimrc
+ln -s rc/zshrc .zshrc
+ln -s rc/p10k.zsh .p10k.zsh
 
-git clone https://github.com/ryx2/ohmyzsh.git
+vim +PlugInstall +qall
+python3 .vim/plugged/youcompleteme/install.py
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git
 mv ohmyzsh .oh-my-zsh
-
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-sudo apt install python3-pip bashtop htop neofetch docker.io vim-gtk3 npm
-
 sudo npm i -g gitmoji-cli
 
-zsh
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zdharma/fast-syntax-highlighting.git ~ZSH_CUSTOM/plugins/fast-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma/fast-syntax-highlighting.git ~${ZSH_CUSTOM:$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
